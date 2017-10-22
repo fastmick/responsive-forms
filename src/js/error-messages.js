aee4327bc8d20c5a8ebc01141b48356296143f77
fastmick <michaelhoffmann@gmx.org>
2017-10-22 15:43:25 +0200

    js aufgeräumt

--- src/js/error-messages.js
+++ src/js/error-messages.js
@@ -1,31 +1,53 @@
-(function () {
-    
-    $("button").click(function () {
-        var invalidFields = $('form :invalid');
-        var errorMessages = $('form .error-message');
-        // console.log('invalidFields (jquery): '+invalidFields.length);
-        // console.log('errorMessages (jquery): '+errorMessages.length);
-        // console.log('invalidField 1 (jquery): '+invalidFields[ 1 ].validationMessage);
-        // var elems = $('form').find(':invalid');
+(function() {
+    $("button").click(function() {
+        // console.log('button click')
+        var browser = $("html").attr("data-browser"),
+            counter = 1;
 
+        // counter for invalidFields must be start with 0
+        if (browser == "edge") {
+            counter = 0;
+        }
+
+        var invalidFields = $("form :invalid"),
+            errorMessages = $("form .error-message"),
+            errorMessage = invalidFields[counter].validationMessage;
+
+        // console.log('errorMessages: ' + errorMessages.length);
+
+        // -> show and delete only one error message
+        // delete error message
+        // var parentTagErrorMessage = $(errorMessages[0]).parent();
+        // parentTagErrorMessage.children('.error-message').remove();
+
+        // // create error message
+        // var parentTagInvalidInput = $(invalidFields[counter]).parent();
+        // parentTagInvalidInput.append(
+        //     "<div class='error-message bubble with-border bubble__arrow-top bubble__arrow-pos-x-12'><i class='fa fa-exclamation-triangle' aria-hidden='true'></i>" +
+        //     errorMessage +
+        //     "</div>"
+        // );
+
+        // -> show and delete all error messages
         for (var i = 0; i < errorMessages.length; i++) {
             // console.log('remove: '+errorMessages.length);
             var parentTag = $(errorMessages[i]).parent();
-            parentTag.children('.error-message').remove();
+            parentTag.children(".error-message").remove();
         }
 
-        for (var i = 1; i < invalidFields.length; i++) {
+        for (var i = counter; i < invalidFields.length; i++) {
             // console.log('invalidField in for (jquery): '+invalidFields[ i ]);
             var parentTag = $(invalidFields[i]).parent();
             var errorMessage = invalidFields[i].validationMessage;
-            parentTag.append("<div class='error-message bubble with-border bubble__arrow-top bubble__arrow-pos-x-12'><i class='fa fa-exclamation-triangle' aria-hidden='true'></i>" + errorMessage + "</div>");
+            parentTag.append(
+                "<div class='error-message bubble with-border bubble__arrow-top bubble__arrow-pos-x-12'><i class='fa fa-exclamation-triangle' aria-hidden='true'></i>" +
+                errorMessage +
+                "</div>"
+            );
         }
 
-        if (invalidFields.length > 0) {
-            console.log('form errors: ' + invalidFields.length);
-        }
+        // if (invalidFields.length > 0) {
+        //     console.log('form errors: ' + invalidFields.length);
+        // }
     });
-
-
-
 })();
